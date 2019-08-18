db315e74e06648819b90a80ae2e7b5f4b00ae5b6
shoai <shoai@DESKTOP-TSOK76B>
2019-08-18 13:46:33 +0530

    First Commit

---
+++ ticekts/src/main/java/com/team/project/ticekts/App.java
@@ -0,0 +1,13 @@
+package com.team.project.ticekts;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
